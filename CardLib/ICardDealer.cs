63dc76157fb55cca6fc56a3365515fce4beb4fe0
l337haxor <47485167+l337haxor@users.noreply.github.com>
2020-03-07 20:11:33 -0500

    new classes

---
+++ CardLib/ICardDealer.cs
@@ -0,0 +1,33 @@
+﻿/* ICardDealer.cs  - Defines the ICardDealer class
+ * 
+ * Author: Sterling Wenzelbach
+ * Since: 2020-03-07
+ * 
+ * Source: 
+          
+ */
+using ClassLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardLib
+{
+    public interface ICardDealer
+    {
+        /// <summary>
+        /// Shuffles the deck
+        /// </summary>
+        void Shuffle();
+
+        /// <summary>
+        /// Deals a card
+        /// </summary>
+        /// <returns>a card</returns>
+        Card DealCard();
+
+        ////
+    }
+}
